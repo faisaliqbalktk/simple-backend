86aa8d98241bc006ccc8a57a68997476a50f98e7
faisal iqbal <faisaliqbal90909@gmail.com>
2024-10-26 08:08:11 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+const port = 4000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/twitter', (req, res) => {
+    res.send('faisal developer in twitter')
+})
+
+app.get('/login', (req, res) => {
+    res.send('<h1>this is login page</h1>')
+})
+
+app.get('/youtuber', (req, res) => {
+    res.send('<h1>hello faisu youtuber</h1>')
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+})
